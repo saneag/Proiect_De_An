23e5a6118c94b062e833eac99044756844951285
saneag <garstea2002@mail.ru>
2021-11-04 16:42:38 +0200

    pe jumate gata

---
+++ site/cars.js
@@ -0,0 +1,109 @@
+
+class Car {
+    constructor(options) {
+        this.marca = options.marca
+        this.model = options.model
+        this.anul = options.anul
+        this.motor = options.motor
+        this.combustibil = options.combustibil
+        this.capacitatea = options.capacitatea
+        this.puterea = options.puterea
+        this.tractiunea = options.tractiunea
+        this.cutia = options.cutia
+        this.viteze = options.viteze
+        this.pret = options.pret
+    }
+    method() {
+
+    }
+}
+
+const allCars = [];
+
+allCars[0] = new Car({
+    marca: 'Alfa Romeo',
+    model: 'Stelvio Quadrifoglio',
+    anul: '2018',
+    motor: 'V6',
+    combustibil: 'benzin',
+    capacitatea: '2.9 l',
+    puterea: '505 puterea',
+    tractiunea: '4x4',
+    cutia: 'Automat',
+    viteze: '8',
+    pret: '80.000 $'
+});
+
+allCars[1] = new Car({
+    marca: 'Alfa Romeo',
+    model: 'Giulia Quadrifoglio',
+    anul: '2017',
+    motor: 'V6',
+    combustibil: 'benzin',
+    capacitatea: '2.9 l',
+    puterea: '505 puterea',
+    tractiunea: 'Rear',
+    cutia: 'Automat',
+    viteze: '8',
+    pret: '87.000 $'
+});
+
+allCars[2] = new Car({
+
+});
+
+allCars[3] = new Car({
+
+});
+
+allCars[4] = new Car({
+
+});
+allCars[5] = new Car({
+
+});
+allCars[6] = new Car({
+
+});
+allCars[7] = new Car({
+
+});
+allCars[8] = new Car({
+
+});
+allCars[9] = new Car({
+
+});
+allCars[10] = new Car({
+
+});
+allCars[11] = new Car({
+
+});
+allCars[12] = new Car({
+
+});
+allCars[13] = new Car({
+
+});
+allCars[14] = new Car({
+
+});
+allCars[15] = new Car({
+
+});
+allCars[16] = new Car({
+
+});
+allCars[17] = new Car({
+
+});
+allCars[18] = new Car({
+
+});
+allCars[19] = new Car({
+
+});
+allCars[20] = new Car({
+
+});
--- site/script.js
+++ site/script.js
@@ -1,45 +1,68 @@
-window.onload = () => {
-    display()
+let cars = document.getElementsByClassName('cars')
+let myModal = document.getElementById('myModal')
+let modal_content = document.getElementById('modal_content')
+let par, car_info
+let img = document.createElement('img')
+let new_div = document.createElement('div')
+
+for (let i = 0; i < cars.length; i++) {
+    cars[i].addEventListener('click', () => {
+        display(i)
+    })
 }
 
-class Cars {
-    constructor(marca, model, greutate, volum, culoare) {
-        this.marca = marca
-        this.model = model
-        this.greutate = greutate
-        this.volum = volum
-        this.culoare = culoare
-    }
-}
-
-let arr = []
-arr[0] = new Cars('bmw', 'm5', 2500, 3.2, 'alb')
-arr[1] = new Cars('bmw', 'm4', 3000, 5, 'negru')
-arr[2] = new Cars('bmw', 'm3', 2200, 2, 'galben')
-arr[3] = new Cars('bmw', 'm4', 2800, 6.2, 'albastru')
-
-
-function Sort() {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i].greutate < arr[j].greutate) {
-                let temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-            }
-        }
-    }
-    element.replaceWith(element)
-    display()
-}
-
-let par, car_info, element
-function display() {
-    for (let i = 0; i < arr.length; i++) {
-        par = document.createElement("p")
-        car_info = document.createTextNode(arr[i].marca + ' ' + arr[i].model + ' ' + arr[i].greutate)
+function display(x) {
+    myModal.style.visibility = 'visible'
+    myModal.style.opacity = '1'
+    let id = 'car' + (x + 1)
+    let txt = ''
+    display_img(x)
+    new_div = document.createElement('div')
+    for (let i in allCars[x]) {
+        txt = i.charAt(0).toUpperCase() + i.slice(1) + ': ' + allCars[x][i] + '\n'
+        par = document.createElement('p')
+        car_info = document.createTextNode(txt)
         par.appendChild(car_info)
-        element = document.getElementById("cars")
-        element.appendChild(par)
+        new_div.appendChild(par)
+        modal_content.appendChild(new_div)
+    }
+
+}
+function display_img(i) {
+    new_div = document.createElement('div')
+    switch (i) {
+        case 0:
+            img.src = './images/alfa_romeo_stelvio_quadrifoglio_modal.png'
+            new_div.appendChild(img)
+            modal_content.appendChild(new_div)
+            break
+        case 1: img.src = './images/alfa_romeo_giulia_quadrifoglio_modal.jpg'
+            new_div.appendChild(img)
+            modal_content.appendChild(new_div)
+    }
+}
+
+function remove_info() {
+    myModal.style.visibility = 'hidden'
+    myModal.style.opacity = '0'
+    while (modal_content.firstChild) {
+        modal_content.removeChild(modal_content.lastChild)
+    }
+}
+
+window.onclick = (event) => {
+    if (event.target == myModal)
+        remove_info()
+}
+
+function change_theme() {
+    let img = document.getElementById('theme_changer')
+    if (img.src.match('color_change_white.png')) {
+        img.src = './images/color_change_dark.png'
+        document.body.classList.add('change_color')
+    }
+    else {
+        img.src = './images/color_change_white.png'
+        document.body.classList.remove('change_color')
     }
 }
